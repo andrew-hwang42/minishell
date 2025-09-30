503296ee246ca05198719a3f5ce5bcb1ba12dc75
andrew-hwang42 <mh3052611@gmail.com>
2025-09-30 04:12:27 +0200

    20250930_04:12

--- srcs/parse/check_token_pos.c
+++ srcs/parse/check_token_pos.c
@@ -6,30 +6,71 @@
 /*   By: ahwang <ahwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 05:23:58 by ahwang            #+#    #+#             */
-/*   Updated: 2025/09/30 03:37:59 by ahwang           ###   ########.fr       */
+/*   Updated: 2025/09/30 04:03:49 by ahwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incs/minishell.h"
 
+int	check_pos_front(char *line, int i, char token)
+{
+	int	flag_redir_double;
+
+	flag_redir_double = 0;
+	if (line[i + 1] && (line[i + 1] == '<' || line[i + 1] == '>'))
+		flag_redir_double = 1;
+	if (i == 0)
+	{
+		if (flag_redir_double)
+			return (token_err_msg(token, token), 0);
+		return (token_err_msg(token, 0), 0);
+	}
+	i--;
+	while (i > 0 && line[i] == ' ')
+		i--;
+	if (i == 0 || line[i] == '|'
+		|| line[i] == '<' || line[i] == '>')
+	{
+		if (flag_redir_double)
+			return (token_err_msg(token, token), 0);
+		return (token_err_msg(token, 0), 0);
+	}
+	return (1);
+}
+
+int	check_pos_back(char *line, int *i, char token)
+{
+	(*i)++;
+	if (line[*i] == token)
+	{
+		(*i)++;
+		if (line[*i] == '\0')
+			return (token_err_msg(token, token), 0);
+		while (line[*i] == ' ')
+			(*i)++;
+		if (line[*i] == '|' || line[*i] == '<' || line[*i] == '>')
+			return (token_err_msg(token, token), 0);
+	}
+	else
+	{
+		if (line[*i] == '\0')
+			return (token_err_msg(token, 0), 0);
+		while (line[*i] == ' ')
+			(*i)++;
+		if (line[*i] == '|' || line[*i] == '<' || line[*i] == '>')
+			return (token_err_msg(token, 0), 0);
+	}
+	return (1);
+}
+
 int	check_pos_err(char *line, int *i)
 {
 	char	token;
-	char	token_next;
 
 	token = line[*i];
-	line++;
-	token_next = 0;
-	if ((line[*i] == '>' || line[*i] == '<') && line[*i] == token)
-	{
-		token_next = line[*i];
-		(*i)++;
-	}
-	while (line[*i] == ' ')
-		(*i)++;
-	if (line[*i] == '\0'
-		|| line[*i] == '|' || line[*i] == '>' || line[*i] == '<')
-		return (token_err_msg(token, token_next), 0);
+	if (!check_pos_front(line, *i, token)
+		|| !check_pos_back(line, i, token))
+		return (0);
 	return (1);
 }
 
